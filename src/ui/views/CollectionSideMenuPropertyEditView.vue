c1b772a1b6f0dafa69150006db7c57ed999c0cec
het-t <hettarkhala068@gmail.com>
2024-01-22 01:29:28 +0530

    feat(collection-side-menu):restructuring

--- src/ui/views/CollectionSideMenuPropertyEditView.vue
+++ src/ui/views/CollectionSideMenuPropertyEditView.vue
@@ -8,7 +8,7 @@
 
         </template>
 
-        <template #mid-part>
+        <template #mid-part>      
             <div style="padding-top: 6px; padding-bottom: 6px;">
                 <div style="display: flex; align-items: center; line-height: 120%; width: 100%; user-select: none; min-height: 28px; font-size: 14px;">
                     <div style="margin-left: 12px; margin-right: auto; min-width: 0; flex-shrink: 0;">
@@ -21,7 +21,7 @@
                         <div style="display: flex;">
                             <div class="xdoc-focusable-within" style="display: flex; align-items: center; width: 100%; font-size: 14px; line-height: 20px; padding: 3px 6px; position: relative; border-radius: 4px; box-shadow: rgba(15, 15, 15, 0.1) 0 0 0 1px inset; background: rgba(242, 241, 238, 0.6); cursor: text; height: 28px;">
                                 <input 
-                                    :value="state.propertyName"
+                                    :value="propertyEdit.name"
                                     type="text" 
                                     placeholder="Property name" 
                                     style="font-size: inherit; line-height: inherit; border: none; background: none; width: 100%; display: block; resize: none; padding: 0px;"
@@ -43,41 +43,69 @@
 
                     <template #itemMeta>
                         <svg role="graphics-symbol" viewBox="0 0 16 16" class="typesText" style="width: 14px; height: 14px; display: block; fill: inherit; flex-shrink: 0; margin-right: 6px;"><path d="M1.56738 3.25879H14.4258C14.7676 3.25879 15.0479 2.97852 15.0479 2.63672C15.0479 2.29492 14.7744 2.02148 14.4258 2.02148H1.56738C1.21875 2.02148 0.952148 2.29492 0.952148 2.63672C0.952148 2.97852 1.22559 3.25879 1.56738 3.25879ZM1.56738 6.84082H14.4258C14.7676 6.84082 15.0479 6.56055 15.0479 6.21875C15.0479 5.87695 14.7744 5.60352 14.4258 5.60352H1.56738C1.21875 5.60352 0.952148 5.87695 0.952148 6.21875C0.952148 6.56055 1.22559 6.84082 1.56738 6.84082ZM1.56738 10.4229H14.4258C14.7676 10.4229 15.0479 10.1426 15.0479 9.80078C15.0479 9.45898 14.7744 9.18555 14.4258 9.18555H1.56738C1.21875 9.18555 0.952148 9.45898 0.952148 9.80078C0.952148 10.1426 1.22559 10.4229 1.56738 10.4229ZM1.56738 14.0049H8.75879C9.10059 14.0049 9.38086 13.7246 9.38086 13.3828C9.38086 13.041 9.10742 12.7676 8.75879 12.7676H1.56738C1.21875 12.7676 0.952148 13.041 0.952148 13.3828C0.952148 13.7246 1.22559 14.0049 1.56738 14.0049Z"></path></svg>
-                        {{ state.propertyType }}
+                        {{ propertyEdit.type }}
                     </template>
                 </base-collection-side-menu-item-col-3>
+
+                <collection-side-menu-property-edit-multiselect
+                    v-if="propertyEdit.type === 'multi_select'"
+                    :property-id="propertyEditId"
+                    :options="propertyEdit.options"
+                />
+            </div>
+
+        </template>
+
+        <template #footer>
+            <div style="padding-top: 6px; padding-bottom: 6px; box-shadow: rgba(55, 53, 47, 0.09) 0px -1px 0px; margin-top: 1px;">
+                footer comes here
             </div>
         </template>
     </base-collection-side-menu>
 </template>
 
 <script setup>
-import { computed, onMounted, reactive } from 'vue';
-import BaseButton from './BaseButton.vue';
-import BaseCollectionSideMenu from './BaseCollectionSideMenu.vue';
-import BaseCollectionSideMenuItemCol3 from './BaseCollectionSideMenuItemCol3.vue';
+import { defineProps } from 'vue';
+import BaseButton from '@/ui/components/BaseButton.vue';
+import BaseCollectionSideMenu from '@/ui/components/BaseCollectionSideMenu.vue';
+import BaseCollectionSideMenuItemCol3 from '@/ui/components/BaseCollectionSideMenuItemCol3.vue';
+import CollectionSideMenuPropertyEditMultiselect from '../components/CollectionSideMenuPropertyEditMultiselect.vue';
 import { useCollectionsStore } from '@/stores/collections';
+import { useRecordValuesStore } from '@/stores/recordValues';
 
-const state = reactive({
-    propertyName: '',
-    propertyType: ''
+const props = defineProps({
+    collectionId: {
+        type: String,
+        required: true
+    },
+    collectionViewId: {
+        type: String,
+        required: true
+    }
 })
+
+const recordValuesStore = useRecordValuesStore();
+
+const collectionProperties = recordValuesStore.getRecordValue(
+    props.collectionId,
+    "collection",
+    "f2cf1fd1-8789-4ddd-9190-49f41966c446"
+).getSchema();
+
+const collectionViewVisibleProperties = recordValuesStore.getRecordValue(
+    props.collectionViewId,
+    "collection_view",
+    "f2cf1fd1-8789-4ddd-9190-49f41966c446"
+).getProperties();
+
+console.log(collectionViewVisibleProperties);
 
 const collectionStore = useCollectionsStore();
 
-const propertyEditData = computed(function () {
-    return collectionStore.getCollectionPropertyEditData;
-})
+const propertyEditId = collectionStore.getCollectionPropertyEditId;
+const propertyEdit = collectionProperties[propertyEditId];
 
 function handlePropertyTypeChange() {
-    console.log("CollectionSideMenuPropertyEdit.vue => handlePropertyTypeChange()")
     collectionStore.setCurrentComponent("propertyTypes")
 }
-
-onMounted(function() {
-    if (propertyEditData.value?.name && propertyEditData.value?.type) {
-        state.propertyName = propertyEditData.value.name;
-        state.propertyType = propertyEditData.value.type;
-    }
-})
 </script>
